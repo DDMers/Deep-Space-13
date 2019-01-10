8a7723263b1398302f3896e60ca877c771d25a4c
Unknown <francismcraeiv@gmail.com>
2019-01-10 09:10:04 -0500

    Update build tools

--- tools/travis/build_byond.sh
+++ tools/travis/build_byond.sh
@@ -47,9 +47,9 @@ if [ "$BUILD_TOOLS" = false ]; then
 
     source $HOME/BYOND/byond/bin/byondsetup
 	if [ "$BUILD_TESTING" = true ]; then
-		tools/travis/dm.sh -DTRAVISBUILDING -DTRAVISTESTING -DALL_MAPS tgstation.dme
+		tools/travis/dm.sh -DTRAVISBUILDING -DTRAVISTESTING -DALL_MAPS Deep-Space-13.dme
 	else
-		tools/travis/dm.sh -DTRAVISBUILDING tgstation.dme
+		tools/travis/dm.sh -DTRAVISBUILDING Deep-Space-13.dme
 		
 		tools/deploy.sh travis_test
 		mkdir travis_test/config
@@ -72,7 +72,7 @@ if [ "$BUILD_TOOLS" = false ]; then
 	
 		cd travis_test
     	ln -s $HOME/libmariadb/libmariadb.so libmariadb.so
-		DreamDaemon tgstation.dmb -close -trusted -verbose -params "test-run&log-directory=travis"
+		DreamDaemon Deep-Space-13.dmb -close -trusted -verbose -params "test-run&log-directory=travis"
 		cd ..
 		cat travis_test/data/logs/travis/clean_run.lk
 
